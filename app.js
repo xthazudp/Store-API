30186eea5723a4d5b157e3796d1ea7d854a992fb
Sudip Shrestha <xthazudp@gmail.com>
2023-02-10 19:52:44 +0545

    connecting server

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('04 Store API')
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+  res.send(`<h1>Store API</h1><a href="/api/v1/products">Products Routes</a>`);
+});
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    // connectDB
+    app.listen(port, console.log(`Server is listening at port ${port}..`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
